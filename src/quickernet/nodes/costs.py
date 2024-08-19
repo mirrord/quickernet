ee49820a81e07c932edcf71421e9497fb8df9238
mirrord <mirrord@gmail.com>
2024-08-19 12:34:00 -0400

    moved dividing by batch size from cost subclasses to CostFunction __call__ parent

--- src/quickernet/nodes/costs.py
+++ src/quickernet/nodes/costs.py
@@ -5,7 +5,7 @@ from .optimization import OptimizableFunction
 
 class CostFunction(OptimizableFunction):
     def __call__(self, inputs, expected_output):
-        return self.forward(inputs, expected_output)
+        return self.forward(inputs, expected_output) / inputs.shape[0]
 
     def forward(self, inputs, expected_output):
         raise NotImplementedError
@@ -20,26 +20,26 @@ class CostFunction(OptimizableFunction):
 # is quadratic vs diffsquares
 class QuadraticCost(CostFunction):
     def forward(self, inputs, expected_output):
-        return 0.5 * np.sum(np.square(inputs - expected_output)).item() / inputs.shape[0]
+        return 0.5 * np.sum(np.square(inputs - expected_output)).item()
 
     def backward(self, inputs, expected_output):
         return inputs - expected_output
 
 class DiffSquaresCost(CostFunction):
     def forward(self, inputs, expected_output):
-        return np.sum(np.square(inputs-expected_output)).item() / inputs.shape[0]
+        return np.sum(np.square(inputs-expected_output)).item()
     def backward(self, inputs, expected_output):
         return 2*(inputs-expected_output)
 
 class CrossEntropyCost(CostFunction):
     def forward(self, inputs, expected_output):
-        return -1*np.sum( expected_output*np.log(inputs) + (1-expected_output)*np.log(1-inputs) ).item() / inputs.shape[0]
+        return -1*np.sum( expected_output*np.log(inputs) + (1-expected_output)*np.log(1-inputs) ).item()
     def backward(self, inputs, expected_output):
         return (inputs-expected_output)/(inputs*(1-inputs))
 
 class HellingerCost(CostFunction):
     def forward(self, inputs, expected_output):
-        return 0.70710678*np.sum( np.square(np.sqrt(inputs)-np.sqrt(expected_output)) ).item() / inputs.shape[0]
+        return 0.70710678*np.sum( np.square(np.sqrt(inputs)-np.sqrt(expected_output)) ).item()
     def backward(self, inputs, expected_output):
         rooty = np.sqrt(inputs)
         return (rooty - np.sqrt(expected_output))/(1.41421356*rooty)
